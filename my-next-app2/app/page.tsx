4d87d6ead0a0dacadf51daa784210c1ca8aeb6a1
shri harini <shri13anitha@gmail.com>
2024-05-24 15:10:13 +0530

    wrote a new code

--- my-next-app2/app/page.tsx
+++ my-next-app2/app/page.tsx
@@ -1,3 +1,16 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+import React from 'react';
+
+interface Props {
+  name: string;
 }
+
+const ExampleComponent: React.FC<Props> = ({ name }) => {
+  return (
+    <div>
+      <h1>Hello, {name}!</h1>
+      <p>This is a simple example component.</p>
+    </div>
+  );
+};
+
+export default ExampleComponent;
